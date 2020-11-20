2941b379ba96b5830d0371ab02570c79f45dc616
Francesco Lazzarotto <fralaz1971@gmail.com>
2020-11-20 20:15:18 +0100

    up

--- plothyperbola.sh
+++ plothyperbola.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./hyperbola -50 50 1 | gnuplot -p -e "set term png;set output 'hyperbola.png';set xlabel 'x'; set ylabel 'y'; set title 'Hyperbola:  x = 1/x'; plot '-' with lines"
+./hyperbola -50 50 1 | gnuplot -p -e "set term png;set output 'hyperbola.png';set xlabel 'x'; set ylabel 'y'; set title 'Hyperbola:  y = 1/x'; plot '-' with lines"
