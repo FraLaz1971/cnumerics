6cab9ad80b46741b4d3f8f72c92a01923938640a
Francesco Lazzarotto <fralaz1971@gmail.com>
2021-02-11 11:38:12 +0100

    added simple check on numerical limits program

---
+++ test_limits.c
@@ -0,0 +1,15 @@
+/* template program  to test some
+numerical limit  */
+
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+#include <stdio.h>
+
+int main(){
+	printf("CHAR_BIT = %d\n", CHAR_BIT);
+	printf("SHRT_MIN = %d\n", SHRT_MIN);
+	printf("SHRT_MAX = %d\n", SHRT_MAX);
+	printf("\n");
+	return 0;
+}
