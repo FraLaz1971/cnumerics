dd787da4fd8f7fdf2a8b79bc9dc77a3a1b17eca8
Francesco Lazzarotto <fralaz1971@gmail.com>
2021-03-04 19:00:13 +0100

    improved A=0 checking

--- solvequad.c
+++ solvequad.c
@@ -6,8 +6,9 @@
 * Get the coefficients of the quadratic equation. */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+
+
 
 int main(){
         float A,B,C,Discr; int ret;
@@ -23,10 +24,10 @@ do {
         printf("(with A not eq. to 0) :");
 	    ret = scanf("%f %f %f", &A, &B, &C);
 /* if A > 0 continue */
-        if (sqrt((A - 0)*(A - 0)) >= 0.000001) break;
+        if ( (fabs(A) - 0)  >= 0.0000009) break;
         printf("Illegal value for coeff. A\n");
         printf("A = %2.9g\n", sqrt((A - 0)*(A - 0)) );
-} while (sqrt((A - 0)*(A - 0)) < 0.0000001);  
+} while ( (fabs(A) - 0) < 0.0000009);  
 /* request input until A is > 0 */
 	    
 /*
@@ -40,15 +41,15 @@ do {
 	printf("Discriminant (= B^2 - 4*A*C) is %g\n", Discr);
 	if ( Discr  <  0) {
 		printf("This equation has complex roots:\n");
-		printf("X_1 = %g+i*%g\n", -B/(2.*A), sqrt(abs(Discr))/(2.*A) );
-		printf("X_2 = %g-i*%g\n", -B/(2.*A), sqrt(abs(Discr))/(2.*A) );
+		printf("X_1 = %g+i*%g\n", -B/(2.*A), sqrt(fabs(Discr))/(2.*A) );
+		printf("X_2 = %g-i*%g\n", -B/(2.*A), sqrt(fabs(Discr))/(2.*A) );
     } else if ( sqrt((pow(B,2) - 4.*A*C - 0)*(pow(B,2) - 4.*A*C - 0)) < 0.0000001) {
             printf("Equation with single repeated real root:\n");
             printf("X_1 = X_2 =  %g\n", -B/(2.*A) );
     } else {
 		printf("Equation with two distinct real roots:\n");
-		printf("X_1 =%g\n", (-B + sqrt(abs(Discr)))/(2.*A));
-		printf("X_2 =%g\n", (-B - sqrt(abs(Discr)))/(2.*A));
+		printf("X_1 =%g\n", (-B + sqrt(fabs(Discr)))/(2.*A));
+		printf("X_2 =%g\n", (-B - sqrt(fabs(Discr)))/(2.*A));
 	}
 /* * */
     return 0;
