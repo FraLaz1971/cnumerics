46f2793af4698e04a2c8f5647e136c3d43ed32d3
Francesco Lazzarotto <fralaz1971@gmail.com>
2022-01-16 00:42:32 +0100

    updated install.bat

--- install.bat
+++ install.bat
@@ -1,9 +1,13 @@
+REM this script is not complete
+REM Date: 2021-01-16
+REM Author: Francesco Lazzarotto mailto:fralaz1971@gmail.com
 bash configure
-#cmd /c configure.bat
+REM to be developed cmd batch configure.bat
+REM cmd /c configure.bat
 mingw32-make
 mingw32-make install
 mingw32-make clean
-cp plot*.* bin
+copy plot*.* bin
 PAUSE
 
 
